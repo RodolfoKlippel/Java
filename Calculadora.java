b8322e48b861b3b9ccabb8279db77ee806ae29da
Rodolfo Klippel <dolforr@gmail.com>
2025-09-18 11:31:36 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Dono do Computador
+ */
+public class Calculadora {
+
+    public static void main(String[] args){
+        
+        Calcular calcular = new Calcular();
+        
+    }
+}
